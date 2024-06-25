edb90c73a6ce2deafce47a1d3d524d8833ad5006
shuta.yasui <shuta_yasui@gdo.co.jp>
2024-06-26 08:48:38 +0900

    EditServlet.javaを作成

--- src/models/Message.java
+++ src/models/Message.java
@@ -69,6 +69,9 @@ public class Message {
         this.content = content;
     }
 
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
 
     public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
